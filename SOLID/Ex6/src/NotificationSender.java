a22a24f54a2b1e9c9936dd88fb9e5d80ee11fe95
Shifa <3639shifa@example.com>
2026-03-05 13:53:21 +0530

    Answer-6

--- SOLID/Ex6/src/NotificationSender.java
+++ SOLID/Ex6/src/NotificationSender.java
@@ -3,3 +3,8 @@ public abstract class NotificationSender {
     protected NotificationSender(AuditLog audit) { this.audit = audit; }
     public abstract void send(Notification n);
 }
+//LSP??
+// how to inherit properly?? for this puprpose it is there
+// if wrongly inheritr=ed then  LSP violated
+// if i have Class C which has void x(){..}
+// i inherited C1 extends C and C2 extends C then C1.x() abs c2.x() should not break
